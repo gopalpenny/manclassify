9cab9386b2a7fbff5214d42643379d7714a570d7
Gopal Penny <gopalpenny@Gopals-MacBook-Pro.local>
2022-08-22 13:45:31 +0800

    initialize classDF with loc_id from 0 to nrows -1

--- appmodules/ClassifyPageFunctions.py
+++ appmodules/ClassifyPageFunctions.py
@@ -54,7 +54,7 @@ def InitializeClassDF():
     elif st.session_state['status']['random_status']:
         loc = gpd.read_file(random_path).to_crs(4326)
         # print(pd.DataFrame(loc['loc_id']))
-        class_df = pd.DataFrame(loc['loc_id']) #.drop(['geometry'], axis = 1)
+        class_df = pd.DataFrame({'loc_id':np.arange(loc.shape[0])}) #.drop(['geometry'], axis = 1)
         class_df['Class'] = np.nan
         
         for i in range(len(subclass_years)):
