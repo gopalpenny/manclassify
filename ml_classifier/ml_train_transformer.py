da0557b16d89c5942f680652d42c368db84887e0
Gopal Penny <gopalpenny@gmail.com>
2023-01-14 22:13:14 +0800

    progress with spyder transformer

--- ml_classifier/ml_train_transformer.py
+++ ml_classifier/ml_train_transformer.py
@@ -8,11 +8,14 @@ Created on Sat Jan 14 19:18:30 2023
 
 # %%
 import torch
+from torch import nn, Tensor
+from torch.utils.data import DataLoader, WeightedRandomSampler
 import os
-# import sklearn
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import OneHotEncoder
 import numpy as np
+import matplotlib.pyplot as plt
+from ml_classifier.ml_transformer import SentinelDatasets, TransformerClassifier
 
 # %%
 proj_paths = ["/Users/gopal/Google Drive/_Research/Research projects/ML/manclassify/app_data/Thailand",
@@ -21,10 +24,15 @@ proj_paths = ["/Users/gopal/Google Drive/_Research/Research projects/ML/manclass
 proj_path = [path for path in proj_paths if os.path.exists(path)][0]
 
 
-s1 = torch.load(os.path.join(proj_path, 'model_data_s1.pt'))
-s2 = torch.load(os.path.join(proj_path, 'model_data_s2.pt'))
+s1_all = torch.load(os.path.join(proj_path, 'model_data_s1.pt'))
+s2_all = torch.load(os.path.join(proj_path, 'model_data_s2.pt'))
 classes = torch.load(os.path.join(proj_path, 'model_data_classes.pt'))
 classes
+
+# %%
+s1 = None
+s2 = None
+
 # %%
 def int_to_onehot(y, num_labels):
     ary = np.zeros((y.shape[0], num_labels))
@@ -57,37 +65,128 @@ print([int(torch.sum(y_test[:,x + 1]).item()) for x in np.arange(classes_one_hot
 
 # %%
 
-y_test
+# Split training X values
+s1_train = s1_all[torch.isin(s1_all[:, 0], y_train[:,0])]
+s1_valid = s1_all[torch.isin(s1_all[:, 0], y_valid[:,0])]
+s1_test = s1_all[torch.isin(s1_all[:, 0], y_test[:,0])]
+
+s2_train = s2_all[torch.isin(s2_all[:, 0], y_train[:,0])]
+s2_valid = s2_all[torch.isin(s2_all[:, 0], y_valid[:,0])]
+s2_test = s2_all[torch.isin(s2_all[:, 0], y_test[:,0])]
 
 # %%
-# get y_train values from loc_train
-y_train_df = (loc_train.assign(val = 1) \
-  .pivot_table(columns = 'class', index = ['loc_id'], values = 'val', fill_value= 0) \
-  .reset_index(['loc_id']))
-y_train = y_train_df.to_numpy()
-print('y_train:\n',y_train)
 
-# get x_train values from loc_ts_norm (based on loc_id)
-x_train = loc_ts_norm[torch.isin(loc_ts_norm[:,0],torch.tensor(y_train[:,0]).to(torch.float64)),:]
+# Prep datasets
+data_train = SentinelDatasets(s1_train, s2_train, y_train, 64, 64)
+data_valid = SentinelDatasets(s1_valid, s2_valid, y_valid, 64, 64)
+data_test = SentinelDatasets(s1_test, s2_test, y_test, 64, 64)
 
-# get y_test values from loc_test
-y_valid_df = (loc_valid.assign(val = 1) \
-  .pivot_table(columns = 'class', index = ['loc_id'], values = 'val', fill_value= 0) \
-  .reset_index(['loc_id']))
-y_valid = y_valid_df.to_numpy()
-print('y_valid:\n',y_valid[0:10,])
+# %%
 
-# get x_train values from loc_ts_norm (based on loc_id)
-x_valid = loc_ts_norm[torch.isin(loc_ts_norm[:,0],torch.tensor(y_valid[:,0]).to(torch.float64)),:]
+# Prep weighted sampling for training data
 
-# get y_test values from loc_test
-y_test_df = (loc_test.assign(val = 1) \
-  .pivot_table(columns = 'class', index = ['loc_id'], values = 'val', fill_value= 0) \
-  .reset_index(['loc_id']))
-y_test = y_test_df.to_numpy()
-print('y_test:\n',y_test[0:10,])
+# adapted from https://discuss.pytorch.org/t/how-to-handle-imbalanced-classes/11264/2
+target_classes = torch.stack([torch.argmax(data_train.__getitem__(i)[2]) for i in range(data_train.__len__())])
+# count of samples in each class
+class_sample_count = np.array([torch.sum(target_classes == i) for i in torch.unique(target_classes)])
 
-# get x_train values from loc_ts_norm (based on loc_id)
-x_test = loc_ts_norm[torch.isin(loc_ts_norm[:,0],torch.tensor(y_test[:,0]).to(torch.float64)),:]
+# weight for each class (classes must go from 0 to n-1 classes)
+weight = 1. / class_sample_count
+sample_weights = np.array([weight[i] for i in target_classes])
+sampler = WeightedRandomSampler(weights = sample_weights, num_samples = len(sample_weights))
 
-x_test
+
+# %%
+# Prep dataloaders
+
+train_dl = DataLoader(data_train, batch_size = 20, drop_last = True, sampler = sampler)
+valid_dl = DataLoader(data_valid, batch_size = 20, drop_last = False)
+test_dl = DataLoader(data_test, batch_size = 20, drop_last = False)
+
+
+# %%
+
+xnn = TransformerClassifier(64, dmodel = 36, nhead = 6, dhid = 100, nlayers = 3, data_dim = 5, nclasses = 4)
+
+# %%
+# test transformer
+s1, s2, y = next(iter(train_dl))
+xnn(s2)
+
+# %%
+
+def get_num_correct(model_batch_out, y_batch):
+    pred = torch.argmax(model_batch_out, dim = 1)
+    actual = torch.argmax(y_batch, dim = 1)
+    num_correct = torch.sum(pred == actual).item()
+    # print('type',type(num_correct))
+    # x = num_correct# item()
+    # print('num_correct', num_correct.item())
+    return num_correct
+
+
+# %%
+
+# Training loop
+
+# i = 1
+loss_fn = nn.CrossEntropyLoss()
+optimizer = torch.optim.Adam(xnn.parameters(), lr = 0.001)
+
+
+# print(i)
+# for train_features, train_labels in train_dl:
+#     i += 1
+#     print(i)
+n_epochs = 100
+loss_hist_train = [0] * n_epochs
+accuracy_hist_train = [0] * n_epochs
+loss_hist_valid = [0] * n_epochs
+accuracy_hist_valid = [0] * n_epochs
+for epoch in range(n_epochs):
+    xnn.train()
+    print(f"Beginning epoch {epoch+1} / {n_epochs}")
+    for _, x_batch, y_batch in train_dl:
+        
+        # Forward pass
+        pred = xnn(x_batch)
+        loss = loss_fn(pred, y_batch)
+        
+        # Accumulate loss and accuracy
+        loss_hist_train[epoch] += loss.item() * y_batch.size(0)
+        accuracy_hist_train[epoch] += get_num_correct(pred, y_batch)
+
+        # Backward and optimize
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+        
+    loss_hist_train[epoch] /= float(len(train_dl.dataset))
+    accuracy_hist_train[epoch] /= float(len(train_dl.dataset))
+    
+    with torch.no_grad():
+        for _, x_batch, y_batch in train_dl:
+
+            # Forward pass
+            pred = xnn(x_batch)
+            loss = loss_fn(pred, y_batch)
+
+            # Accumulate loss and accuracy
+            loss_hist_valid[epoch] += loss.item() * y_batch.size(0)
+            accuracy_hist_valid[epoch] += get_num_correct(pred, y_batch)
+
+        loss_hist_valid[epoch] /= float(len(train_dl.dataset))
+        accuracy_hist_valid[epoch] /= float(len(train_dl.dataset))
+        
+    print(f'Epoch [{epoch+1}/{n_epochs}], Loss: {loss_hist_train[epoch]:.4f}, Accuracy: {accuracy_hist_train[epoch]:.4f}'
+          f' Val Accuracy: {accuracy_hist_valid[epoch]:.4f}')
+        
+
+# %%
+fig, axs = plt.subplots(2)
+axs[0].plot(loss_hist_train)
+axs[0].plot(loss_hist_valid)
+axs[0].set(ylabel = "Loss")
+axs[1].plot(accuracy_hist_train)
+axs[1].plot(accuracy_hist_valid)
+axs[1].set(ylabel = "Accuracy")
--- ml_classifier/ml_transformer.py
+++ ml_classifier/ml_transformer.py
@@ -8,6 +8,118 @@ Created on Fri Jan 13 21:35:24 2023
 
 from torch.utils.data import Dataset, DataLoader, WeightedRandomSampler
 import torch
+from torch import nn, Tensor
+
+
+# %%
+class SentinelDatasets(Dataset):
+    """Sentinel 2 dataset"""
+    
+    def __init__(self, s1, s2, y, max_obs_s1, max_obs_s2):
+        """
+        Args:
+            s1 (tensor): contains loc_id and predictors as columns, s1 observations as rows
+            s2 (tensor): contains loc_id and predictors as columns, s2 observations as rows
+            y (tensor): contains loc_id as rows (& first column), class as 1-hot columns
+        """
+        self.s1 = s1
+        self.s2 = s2
+        self.y = y
+        self.max_obs_s1 = max_obs_s1
+        self.max_obs_s2 = max_obs_s2
+        # self.proj_path = proj_path
+        # proj_normpath = os.path.normpath(proj_path)
+        # proj_dirname = proj_normpath.split(os.sep)[-1]
+        # self.proj_name = re.sub("_classification$","",proj_dirname)
+        # self.class_path = os.path.join(proj_path, self.proj_name + "_classification")
+        # self.ts_path = os.path.join(proj_path, self.proj_name + "_download_timeseries")
+        # self.pt_classes = pd.read_csv(os.path.join(self.class_path,"location_classification.csv"))
+        # self.pt_classes = classes[['loc_id', class_colname]].dropna()
+        # self.classes = pd.unique(self.pt_classes[class_colname])
+        # self.labels = self.pt_classes.assign(val = 1).pivot_table(columns = class_colname, index = 'loc_id', values = 'val', fill_value= 0)
+
+    
+    def __getitem__(self, idx):
+        # get loc_id
+        loc_id = self.y[idx,0]
+        self.last_loc_id = loc_id
+        
+        # select location id
+        s1_loc = self.s1[self.s1[:,0]==loc_id]
+        s1_prep = s1_loc[:,1:] # remove loc_id column
+        
+        # pad zeros to max_obs
+        n_pad_s1 = self.max_obs_s1 - s1_prep.shape[0]
+        
+        s1 = torch.cat((s1_prep, torch.zeros(n_pad_s1, s1_prep.shape[1])), dim = 0)
+        
+        s1 = s1.float()
+        
+        
+        # select location id
+        s2_loc = self.s2[self.s2[:,0]==loc_id]
+        s2_prep = s2_loc[:,1:] # remove loc_id column
+        
+        # pad zeros to max_obs
+        n_pad_s2 = self.max_obs_s2 - s2_prep.shape[0]
+        
+        s2 = torch.cat((s2_prep, torch.zeros(n_pad_s2, s2_prep.shape[1])), dim = 0)
+        
+        s2 = s2.float()
+        
+        
+        # get one-hot encoding for the point as tensor
+        y = torch.tensor(self.y[idx,1:]).float()
+        
+        return s1, s2, y
+        
+    def __len__(self):
+        return self.y.shape[0]
+    
+    
+class TransformerClassifier(nn.Module):
+    def __init__(self, ntoken: int, dmodel: int, nhead: int, dhid: int, 
+                 nlayers: int, data_dim: int, nclasses: int):
+        """
+        data_dim: dimension of data (i.e., num of columns) including position as first dimension (but not loc_id)
+        """
+        super().__init__()
+        self.positional_layer = nn.Linear(1, dmodel)
+        self.embed_layer = nn.Linear(data_dim - 1, dmodel) # transform data to embed dimension (dmodel)
+        
+        # dim_feedforward: https://stackoverflow.com/questions/68087780/pytorch-transformer-argument-dim-feedforward
+        # shortly: dim_feedforward is a hidden layer between two forward layers at the end of the encoder layer, passed for each word one-by-one
+        self.encoderlayer = nn.TransformerEncoderLayer(d_model = dmodel, nhead = nhead, dim_feedforward = dhid)
+        self.encoder = nn.TransformerEncoder(self.encoderlayer, nlayers)
+        
+        self.num_params = ntoken * dmodel
+        
+        self.class_encoder = nn.Linear(dmodel, nclasses)
+    
+    def forward(self, src: Tensor) -> Tensor:
+        
+        positions = src[:, :, 0:1]
+        data = src[:, :, 1:]
+        pe = self.positional_layer(positions)
+        data_embed = self.embed_layer(data)
+        data_and_pe = pe + data_embed
+        encoder_out = self.encoder(data_and_pe)
+        
+        maxpool = torch.max(encoder_out,dim = 1)[0]
+        
+        # softmax ensures output of model is probability of class membership -- which sum to 1
+        # BUT this is already done with CrossEntropyLoss so it's not necessary for this loss function
+        classes = self.class_encoder(maxpool) #, dim = 1
+        
+        # classes = nn.functional.softmax(classes, 1) # don't use softmax with cross entropy loss... or do?
+        # don't: https://stackoverflow.com/questions/55675345/should-i-use-softmax-as-output-when-using-cross-entropy-loss-in-pytorch
+        # do: Machine Learning with Pytorch and Scikitlearn (p 471: Loss functions for classifiers) -- BUT NOT WITH CROSS ENTROPY LOSS (p478
+        
+        return classes
+
+        # data_in = tf_test[:, :, 1:] # select only the data
+        # positions = tf_test[:,:,0:1] # split out positional data
+        # data_dim = data_in.shape[-1]
 
 class s2Dataset(Dataset):
     """Sentinel 2 dataset"""
